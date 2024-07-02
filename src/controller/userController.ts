0d46b9c92d72b5079c55298086e1b05caed70a53
bruce <euryanspike.pro@gmaiul.com>
2024-07-02 17:52:16 -0300

    inserindo usuário no db + geração de id com uuidv4

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,7 +1,26 @@
 import {Request, Response} from 'express'
+import {pool} from '../config/database';
+import {v4} from 'uuid'
 
-export const registerUser = (req: Request, res: Response) => {
+export const registerUser = async (req: Request, res: Response) => {
     const {name, email, pass, confirmPass} = req.body;
 
-    return res.status(200).json({ message: email })
+    try{
+
+        const insertUserQuery = `INSERT INTO users(id, name, email, pass) VALUES(?,?,?,?)`;
+
+
+        const id = v4();
+        const values = [id, name, email, pass]
+
+        const [data] = await pool.execute(insertUserQuery, values);
+
+
+        return res.status(200).json({ message: 'usuário criado.' })
+
+
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({ message: 'erro no servidor.' })
+    }
 }
